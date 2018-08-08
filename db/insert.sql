27a84d41471a1df4a24c445161745a216c2be3e6
FerValle <fervalle264@gmail.com>
2018-08-08 10:48:38 -0300

    archivos de base de datos
    - Imagen de modelo logico
    - archivo de wrb de modelo logico
    - script creacion de tablas

---
+++ db/insert.sql
@@ -0,0 +1,5 @@
+use proyecto;
+
+INSERT INTO empleados VALUES (1, 'Lourdes', 'Alvarez', 'lulialv', 'psqlulialv', 'v', 'a', null);
+INSERT INTO empleados VALUES (1, 'Alfredo', 'Alvarez', 'lulialv', 'psqlulialv', 'v', 'a', null);
+INSERT INTO empleados VALUES (1, 'Magdalena', 'Sanchez', 'maguisan', 'psqlulialv', 'v', 'a', null);
