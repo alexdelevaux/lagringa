3579c643525bc1c40b9408e299993c0c0453461e
Alejandro <acdelevaux@gmail.com>
2018-08-08 11:42:47 -0300

    Proyecto de Cero de nuevo. incluye la DB básica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
 
 import { AppComponent } from './app.component';
-import { TabsComponent } from './tabs/tabs.component';
-
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TabsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        TableModule,
+        HttpClientModule,
+        InputTextModule,
+        DialogModule,
+        ButtonModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
