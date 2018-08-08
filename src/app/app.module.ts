b7ac194a823690637b3b0e8f9f0e929cca899ce1
Alejandro <acdelevaux@gmail.com>
2018-08-08 10:52:37 -0300

    Creado el componente (Tabs)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule
